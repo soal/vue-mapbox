9ec66c72cdecf37ec6cec655ec35efdf97365fa9
soal <sorrow.about.alice@gmail.com>
2017-04-01 16:42:02 +0300

    Moving props and event list to separate files

---
+++ src/lib/options.js
@@ -0,0 +1,126 @@
+export default {
+  container: {
+    type: [String, HTMLElement],
+    default () {
+      return "map";
+    }
+  },
+  accessToken: {
+    type: String,
+    default: null
+  },
+  minZoom: {
+    type: Number,
+    default: 0
+  },
+  maxZoom: {
+    type: Number,
+    default: 22
+  },
+  mapStyle: {
+    type: [String, Object],
+    required: true
+  },
+  hash: {
+    type: Boolean,
+    default: false
+  },
+  interactive: {
+    type: Boolean,
+    default: true
+  },
+  bearingSnap: {
+    type: Number,
+    default: 7
+  },
+  classes: {
+    type: Array,
+    default () {
+      return [];
+    }
+  },
+  attributionControl: {
+    type: Boolean,
+    default: true
+  },
+  logoPosition: {
+    type: String,
+    default: "bottom-left"
+  },
+  failIfMajorPerformanceCaveat: {
+    type: Boolean,
+    default: false
+  },
+  preserveDrawingBuffer: {
+    type: Boolean,
+    default: false
+  },
+  refreshExpiredTiles: {
+    type: Boolean,
+    default: false
+  },
+  maxBounds: {
+    type: Array,
+    default () {
+      return undefined;
+    }
+  },
+  scrollZoom: {
+    type: [Boolean, Object],
+    default () {
+      return true;
+    }
+  },
+  boxZoom: {
+    type: Boolean,
+    default: true
+  },
+  dragRotate: {
+    type: Boolean,
+    default: true
+  },
+  dragPan: {
+    type: Boolean,
+    default: true
+  },
+  keyboard: {
+    type: Boolean,
+    default: true
+  },
+  doubleClickZoom: {
+    type: Boolean,
+    default: true
+  },
+  touchZoomRotate: {
+    type: [Boolean, Object],
+    default () {
+      return true;
+    }
+  },
+  trackResize: {
+    type: Boolean,
+    default: true
+  },
+  center: {
+    type: Array,
+    default () {
+      return [0, 0];
+    }
+  },
+  zoom: {
+    type: Number,
+    default: 0
+  },
+  bearing: {
+    type: Number,
+    default: 0
+  },
+  pitch: {
+    type: Number,
+    default: 0
+  },
+  renderWorldCopies: {
+    type: Boolean,
+    default: true
+  }
+};

ac15c5c12e6c8ed1281c7757e2eb6b629ed0c2a0
soal <sorrow.about.alice@gmail.com>
2017-04-15 01:29:10 +0300

    Updating build system for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/dist/",
-    filename: "vue-mapbox.min.js",
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'vue-mapbox.min.js',
     // library: "vue-mapbox",
-    libraryTarget: "umd"
+    libraryTarget: 'umd'
   },
-  externals: ["vue", "mapbox-gl"],
+  externals: ['vue', 'mapbox-gl'],
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: 'vue-loader'
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
     alias: {
-      "vue$": "vue/dist/vue.esm.js"
+      'vue$': 'vue/dist/vue.esm.js'
     }
   },
   performance: {
@@ -34,24 +34,22 @@ module.exports = {
   }
 };
 
-if (process.env.NODE_ENV === "production") {
-  module.exports.devtool = "#source-map";
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: "'production'"
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      compress: {
-        warnings: false,
-        comparisons: false  // don't optimize comparisons. IMPORTANT! Optimizing comparsions breaks production build with mapbox-gl. See: https://github.com/mapbox/mapbox-gl-js/issues/4359
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]);
-}
+module.exports.devtool = '#source-map';
+// http://vue-loader.vuejs.org/en/workflow/production.html
+module.exports.plugins = (module.exports.plugins || []).concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: "'production'"
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: false,
+    compress: {
+      warnings: false,
+      comparisons: false  // don't optimize comparisons. IMPORTANT! Optimizing comparsions breaks production build with mapbox-gl. See: https://github.com/mapbox/mapbox-gl-js/issues/4359
+    }
+  }),
+  new webpack.LoaderOptionsPlugin({
+    minimize: true
+  })
+]);

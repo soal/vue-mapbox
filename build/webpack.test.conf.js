32d6b0b98ac7bf038a1d8a456774682053fdc736
soal <sorrow.about.alice@gmail.com>
2017-07-27 16:07:31 +0300

    First working unit test for GlMap With Chromium, PhantomJS not working

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -1,6 +1,4 @@
 // This is the webpack config used for unit tests.
-
-var utils = require('./utils')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var baseConfig = require('./webpack.base.conf')

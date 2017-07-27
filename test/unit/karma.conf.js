283ca21ad7498a66708ed9b9bd66d4bde460e82b
soal <sorrow.about.alice@gmail.com>
2017-07-27 16:13:42 +0300

    Testing with headless chromium

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -12,7 +12,9 @@ module.exports = function (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
     // browsers: ['PhantomJS', 'Chromium'],
-    browsers: ['Chromium'],
+    customLaunchers: { HeadlessChromium: { base: 'Chromium', flags: ['--headless', '--remote-debugging-port=9222'] }},
+    browsers: ['HeadlessChromium'],
+
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],

d3cd1086430d8af9f5480e0245f9874e2227b8ec
soal <sorrow.about.alice@pm.me>
2018-12-02 00:54:27 +0300

    Simplified async map actions registration

--- src/components/map/methods/public/camera.js
+++ src/components/map/methods/public/camera.js
@@ -1,109 +1,40 @@
+const methods = [
+  'setCenter',
+  'fitBounds',
+  'easeTo',
+  'flyTo',
+  'jumpTo',
+  'panBy',
+  'panTo',
+  'setPitch',
+  'setBearing',
+  'rotateTo',
+  'resetNorth',
+  'snapToNorth',
+  'zoomTo',
+  'zoomIn',
+  'zoomOut',
+  'setZoom',
+  'fitScreenCoordinates'
+]
+
+function makeMethods () {
+  const actions = {}
+  methods.forEach(name => {
+    actions[name] = function (...args) {
+      if (this.$_mapAsyncActions) {
+        return this.$_mapAsyncActions[name](...args)
+      }
+    }
+  })
+  return actions
+}
+
 export default {
   methods: {
+    ...makeMethods(),
     cameraForBounds (...args) {
       return this.map.cameraForBounds(...args)
-    },
-
-    setCenter (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.setCenter(...args)
-      }
-    },
-
-    fitBounds (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.fitBounds(...args)
-      }
-    },
-
-    easeTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.easeTo(...args)
-      }
-    },
-
-    flyTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.flyTo(...args)
-      }
-    },
-
-    jumpTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.jumpTo(...args)
-      }
-    },
-
-    panBy (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.panBy(...args)
-      }
-    },
-
-    panTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.panTo(...args)
-      }
-    },
-
-    setPitch (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.setPitch(...args)
-      }
-    },
-
-    setBearing (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.setBearing(...args)
-      }
-    },
-
-    rotateTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.rotateTo(...args)
-      }
-    },
-
-    resetNorth (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.resetNorth(...args)
-      }
-    },
-
-    snapToNorth (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.snapToNorth(...args)
-      }
-    },
-
-    zoomTo (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.zoomTo(...args)
-      }
-    },
-
-    zoomIn (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.zoomIn(...args)
-      }
-    },
-
-    zoomOut (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.zoomOut(...args)
-      }
-    },
-
-    setZoom (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.setZoom(...args)
-      }
-    },
-
-    fitScreenCoordinates (...args) {
-      if (this.$_mapAsyncActions) {
-        return this.$_mapAsyncActions.fitScreenCoordinates(...args)
-      }
     }
   }
 }

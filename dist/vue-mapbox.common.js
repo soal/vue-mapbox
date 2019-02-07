ffdccbff2bb8f9850d0d061b2c6c3f7f2c027cb8
soal <sorrow.about.alice@pm.me>
2019-02-08 01:09:09 +0300

    Added default map styling

--- dist/vue-mapbox.common.js
+++ dist/vue-mapbox.common.js
@@ -2160,6 +2160,11 @@ module.exports = /******/ (function(modules) {
       /***/
     },
 
+    /***/ "83e1": /***/ function(module, exports, __webpack_require__) {
+      // extracted by mini-css-extract-plugin
+      /***/
+    },
+
     /***/ "84f2": /***/ function(module, exports) {
       module.exports = {};
 
@@ -2452,6 +2457,25 @@ module.exports = /******/ (function(modules) {
             : cof(it) == "RegExp")
         );
       };
+
+      /***/
+    },
+
+    /***/ ac42: /***/ function(
+      module,
+      __webpack_exports__,
+      __webpack_require__
+    ) {
+      "use strict";
+      /* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GlMap_vue_vue_type_style_index_0_id_b7a136fa_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+        "83e1"
+      );
+      /* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GlMap_vue_vue_type_style_index_0_id_b7a136fa_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/ __webpack_require__.n(
+        _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GlMap_vue_vue_type_style_index_0_id_b7a136fa_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__
+      );
+      /* unused harmony reexport * */
+      /* unused harmony default export */ var _unused_webpack_default_export =
+        _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_GlMap_vue_vue_type_style_index_0_id_b7a136fa_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default.a;
 
       /***/
     },
@@ -2931,7 +2955,7 @@ module.exports = /******/ (function(modules) {
       // EXTERNAL MODULE: ./node_modules/core-js/modules/web.dom.iterable.js
       var web_dom_iterable = __webpack_require__("ac6a");
 
-      // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"752d9a0d-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/map/GlMap.vue?vue&type=template&id=3e0acdb8&
+      // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"752d9a0d-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/map/GlMap.vue?vue&type=template&id=b7a136fa&scoped=true&
       var render = function() {
         var _vm = this;
         var _h = _vm.$createElement;
@@ -2952,7 +2976,7 @@ module.exports = /******/ (function(modules) {
         }
       ];
 
-      // CONCATENATED MODULE: ./src/components/map/GlMap.vue?vue&type=template&id=3e0acdb8&
+      // CONCATENATED MODULE: ./src/components/map/GlMap.vue?vue&type=template&id=b7a136fa&scoped=true&
 
       // CONCATENATED MODULE: ./node_modules/@babel/runtime/helpers/esm/defineProperty.js
       function _defineProperty(obj, key, value) {
@@ -3684,6 +3708,11 @@ module.exports = /******/ (function(modules) {
       };
       // CONCATENATED MODULE: ./src/components/map/GlMap.vue?vue&type=script&lang=js&
       /* harmony default export */ var map_GlMapvue_type_script_lang_js_ = GlMapvue_type_script_lang_js_;
+      // EXTERNAL MODULE: ./src/components/map/GlMap.vue?vue&type=style&index=0&id=b7a136fa&scoped=true&lang=css&
+      var GlMapvue_type_style_index_0_id_b7a136fa_scoped_true_lang_css_ = __webpack_require__(
+        "ac42"
+      );
+
       // CONCATENATED MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
       /* globals __VUE_SSR_CONTEXT__ */
 
@@ -3795,7 +3824,7 @@ module.exports = /******/ (function(modules) {
         staticRenderFns,
         false,
         null,
-        null,
+        "b7a136fa",
         null
       );
 
@@ -5527,16 +5556,10 @@ module.exports = /******/ (function(modules) {
           }
         }
       };
-      // CONCATENATED MODULE: ./src/pluginMixin.js
-
-      /* harmony default export */ var pluginMixin = {
-        mixins: [withRegistration, withEvents, withSelfEvents]
-      };
       // CONCATENATED MODULE: ./src/main.js
 
       var mglRegistrationMixin = withRegistration;
       var mglControlMixin = controlMixin;
-      var mglPluginMixin = pluginMixin;
       var MglMap = GlMap;
       var MglNavigationControl = NavigationControl;
       var MglGeolocateControl = GeolocateControl;
@@ -5597,13 +5620,6 @@ module.exports = /******/ (function(modules) {
         "mglControlMixin",
         function() {
           return mglControlMixin;
-        }
-      );
-      /* concated harmony reexport mglPluginMixin */ __webpack_require__.d(
-        __webpack_exports__,
-        "mglPluginMixin",
-        function() {
-          return mglPluginMixin;
         }
       );
       /* concated harmony reexport MglMap */ __webpack_require__.d(
--- src/components/map/GlMap.vue
+++ src/components/map/GlMap.vue
@@ -88,3 +88,18 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.mgl-map-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.mapboxgl-map {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  position: absolute;
+}
+</style>

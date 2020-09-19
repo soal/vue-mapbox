4590ef4ebe23c8198b773f14ca7a3fa86c3882b9
Manuel Kasper <mk@neon1.net>
2020-09-19 12:02:25 +0200

    Make bounds prop synced (#136)
    * Make bounds prop sync to changes in map
    * Document synced bounds parameter
    
    Co-authored-by: Manuel Kasper <manuel.kasper@threema.ch>

--- src/components/map/mixins/withPrivateMethods.js
+++ src/components/map/mixins/withPrivateMethods.js
@@ -29,9 +29,12 @@ export default {
           events: ["pitch"],
           prop: "pitch",
           getter: this.map.getPitch.bind(this.map)
+        },
+        {
+          events: ["moveend", "zoomend", "rotate", "pitch"],
+          prop: "bounds",
+          getter: this.map.getBounds.bind(this.map)
         }
-        // TODO: make 'bounds' synced prop
-        // { events: ['moveend', 'zoomend', 'rotate', 'pitch'], prop: 'bounds', getter: this.map.getBounds.bind(this.map) }
       ];
       syncedProps.forEach(({ events, prop, getter }) => {
         events.forEach(event => {

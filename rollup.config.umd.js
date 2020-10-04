748196d37ccf313d22807c8455fb29554ded5d08
soal <soal@soal.red>
2020-10-04 10:53:23 +0300

    Updated rollup configuration

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,31 +1,26 @@
 import css from "rollup-plugin-css-only";
 import { terser } from "rollup-plugin-terser";
-// import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/main.js",
-  external: ["mapbox-gl", "vue", "map-promisified"],
+  external: ["mapbox-gl", "vue"],
   output: [
     {
       file: "dist/vue-mapbox.umd.js",
       name: "vue-mapbox",
-      format: "umd",
-      globals: {
-        "map-promisified": "promisify"
-      }
+      format: "umd"
     },
     {
       file: "dist/vue-mapbox.umd.min.js",
       name: "vue-mapbox",
       format: "umd",
-      plugins: [terser()],
-      globals: {
-        "map-promisified": "map-promisified"
-      }
+      plugins: [terser()]
     }
   ],
   plugins: [
+    nodeResolve({ resolveOnly: ["map-promisified"] }),
     css({ output: "dist/vue-mapbox.css" }),
     babel({ babelHelpers: "runtime" })
   ]

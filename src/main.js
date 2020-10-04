87ab81869fa08281521d080478e810c60bf6daa0
soal <soal@soal.red>
2020-10-04 07:18:11 +0300

    .vue files replaced with plain js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import GlMap from "./components/map/GlMap.vue";
+import GlMap from "./components/map/GlMap";
 
 import NavigationControl from "./components/UI/controls/NavigationControl";
 import GeolocateControl from "./components/UI/controls/GeolocateControl";
 import FullscreenControl from "./components/UI/controls/FullscreenControl";
 import AttributionControl from "./components/UI/controls/AttributionControl";
 import ScaleControl from "./components/UI/controls/ScaleControl";
-import Marker from "./components/UI/Marker.vue";
-import Popup from "./components/UI/Popup.vue";
+import Marker from "./components/UI/Marker";
+import Popup from "./components/UI/Popup";
 
 import GeojsonLayer from "./components/layer/GeojsonLayer";
 import ImageLayer from "./components/layer/ImageLayer";
